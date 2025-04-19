7ea63ee42a880c0f8701a7d698f7f0d3632f29e3
Suhani2127 <155729934+Suhani2127@users.noreply.github.com>
2025-04-19 21:40:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import av
 import numpy as np
 import mediapipe as mp
-from streamlit_webrtc 
-import webrtc_streamer, VideoProcessorBase
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 import pandas as pd
 import time
 import cv2
